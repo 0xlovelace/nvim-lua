8c8529280140b7a51b6eb95b4cc4eb13498e15d3
pickle <pickle@picklepc>
2023-03-13 01:06:34 -0400

    added denols fix of tsserver running with it

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,6 @@
 -- Learn the keybindings, see :help lsp-zero-keybindings
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+local nvim_lsp = require('lspconfig')
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
@@ -18,14 +19,12 @@ lsp.ensure_installed({
     'docker_compose_language_service',
     'html',
     'jsonls',
-    'svlangserver',
     'bashls',
     'clangd',
     'cmake',
     'gopls',
     'intelephense',
     'jedi_language_server',
-    'svelte',
     'denols'
 });
 
@@ -34,6 +33,30 @@ vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
 -- Keybind to Format
 vim.keymap.set('n', '<C-f>', vim.lsp.buf.format)
+
+local on_attach = function(client)
+    if nvim_lsp.util.root_pattern("deno.json")(vim.fn.getcwd()) then
+        if client.name == "tsserver" then
+            client.stop()
+            return
+        end
+    end
+end
+
+vim.g.markdown_fenced_languages = {
+    "ts=typescript"
+}
+
+-- Fix For Deno & TSserver Clash!
+lsp.configure('tsserver', {
+    on_attach = on_attach,
+    single_file_support = true,
+})
+
+lsp.configure('denols', {
+    on_attach = on_attach,
+    single_file_support = false,
+})
 
 lsp.setup()
 
