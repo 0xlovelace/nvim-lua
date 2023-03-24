fc65bbc99416682f19d216e759c837e5e71bc5c3
nomadicpickle <picklesjarr@icloud.com>
2023-03-24 12:39:53 -0400

    added save keybinds & changed some others

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,12 +1,19 @@
 -- Global Keys
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-t>", function() vim.cmd 'Neotree' end)
+vim.keymap.set("n", "<C-t>", function() vim.cmd('Neotree') end)
 
--- Cycle Through Buffers
-vim.keymap.set("n", "<leader>bn", function() vim.cmd('bn') end)
-vim.keymap.set("n", "<leader>bp", function() vim.cmd('bp') end)
-vim.keymap.set("n", "<leader>bw", function() vim.cmd('bw | bn') end)
-vim.keymap.set("n", "<leader>bl", function() vim.cmd('ls') end)
+-- Saving & Exiting Keybinds
+vim.keymap.set("n", "<C-s>", function() vim.cmd('w') end)   -- save buffer
+vim.keymap.set("n", "<C-w>", function() vim.cmd('wq') end)  -- save buffer & exit
+vim.keymap.set("n", "<C-a>", function() vim.cmd('wqa') end) -- save all buffers & exit
+
+-- Buffer Keybinds
+vim.keymap.set("n", "<Tab>", function() vim.cmd('bn') end)           -- Next Buffer alternative
+vim.keymap.set("n", "<leader>bn", function() vim.cmd('bn') end)      -- Next Buffer
+vim.keymap.set("n", "<leader>bp", function() vim.cmd('bp') end)      -- Previous Buffer
+vim.keymap.set("n", "<leader>bw", function() vim.cmd('bw | bn') end) --closes current buffer & tabs to the next one
+vim.keymap.set("n", "<leader>bl", function() vim.cmd('ls') end)      -- list buffers
+vim.keymap.set("n", "<leader>ch", function() vim.cmd('changes') end) -- Shows Changes in current buffer
 
 -- Buffer Finder https://alpha2phi.medium.com/neovim-for-beginners-managing-buffers-91367668ce7
 vim.keymap.set('n', "<leader>bf", function()
