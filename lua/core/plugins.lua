163e9d26f1e328323f215d9374bc3ad8814126be
noahpickle <noah.pickle99@outlook.com>
2023-10-28 11:27:56 -0400

    updated telescope branch

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -14,8 +14,7 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
